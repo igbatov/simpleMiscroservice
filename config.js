1104cd2a16525156ea4b844125faa9bf973910bd
Batov <i.batov@megaplan.ru>
2018-02-19 19:41:17 +0300

    added feedback form upload

--- config.js
+++ config.js
@@ -2,12 +2,27 @@ const server = {
     port: 4000,
 }
 const cors = {
+    credentials: true, // нужно чтобы работала подгрузка фалов через CORS
     origin: 'http://localhost:3000'
 }
 const api = {
     name: 'helpBack',
     trackerLog: '/var/log/syslog',
-    supportEmail: 'support@megaplan.ru'
+    feedbackEmail: 'support@megaplan.ru',
+    feedbackSubject: 'Фидбек с help.megaplan.ru',
+    smtp: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "gugnzjua7ankukhg@ethereal.email", // generated ethereal user
+            pass: "vPzTnWmzagpHpNFP3f"  // generated ethereal password
+        }
+    },
+    upload: {
+        limits: { fileSize: 50 * 1024 * 1024 },
+        safeFileNames: /[^a-zA-Z\d-_\.]/g // обрезать все кроме alphanum, -, _, .
+    }
 }
 
-module.exports = Object.assign({}, {api: api, server: server, cors: cors})
+module.exports = Object.assign({}, {api, server, cors})
--- src/api.js
+++ src/api.js
@@ -1,12 +1,19 @@
 'use strict'
 const status = require('http-status')
 const syslog = require('modern-syslog')
+const fs = require("fs")
+const path = require("path")
+const fileUpload = require('express-fileupload')
+const nodemailer = require('nodemailer')
 
 module.exports = (server, options) => {
     if (!options.name) {
         throw new Error('Set api name in config.js!')
     }
 
+    /**
+     * track user actions
+     */
     server.post('/trackBatch', (req, res, next) => {
         try {
             const str = JSON.stringify(req.body)
@@ -18,4 +25,78 @@ module.exports = (server, options) => {
             next(e)
         }
     })
+
+    /**
+     * Feedback form
+     */
+    server.post('/api/v3/support/feedback', function(req, res) {
+        let transporter = nodemailer.createTransport(options.smtp);
+
+        // setup email data with unicode symbols
+        let mail = {
+            from: req.body.email, // sender address
+            to: options.feedbackEmail,
+            subject:  options.feedbackSubject,
+            html: req.body.text,
+            attachments: req.body.attaches.map((attache) => ({
+                filename: attache.id,
+                path: getFilePath(attache.id)
+            }))
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mail, (error, info) => {
+            // в любом случае удаляем вложение
+            req.body.attaches.forEach((attache) => {
+                fs.unlink(getFilePath(attache.id))
+            })
+
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+
+        res.status(status.OK).json({'status':'ok'})
+    })
+
+    /**
+     * File upload
+     */
+    server.post('/api/file', fileUpload(options.upload), function(req, res) {
+        if (!req.files) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        const file = req.files["files[]"]
+        const filepath = getFilePath(file.md5)
+        file.mv(filepath)
+            .then(
+                () => {
+                    const data = [{
+                        contentType: "File",
+                        id: file.md5,
+                        name: file.name,
+                        extension: file.name.split('.').pop(),
+                        size: fs.statSync(filepath).size,
+                        possibleActions:["act_share"]
+                    }]
+
+                    res.status(status.OK).json({
+                        "meta":{
+                            "status":200,
+                            "errors":[]
+                        },
+                        "data":data
+                    })
+                },
+                err => {
+                    console.error( 'Cannot move file to ', filepath, err );
+                }
+            )
+    });
+
+    function getFilePath(filename) {
+        return path.join(__dirname, "..", "uploads", filename);
+    }
 }
